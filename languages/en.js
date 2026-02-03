141fa11198027779e6cd1b3293febb6a15441cca
valerik197832-crypto <valerik197832@gmail.com>
2026-02-03 13:10:32 +0300

    Add new entries to DICT_EN in languages/en.js

--- languages/en.js
+++ languages/en.js
@@ -67,6 +67,15 @@ var DICT_EN = {
     'povos': { text: 'Will be able', roots: ['can', 'fut'] },
     'ajnan': { text: 'Any', roots: ['any', 'acc'] },
     'vidi': { text: 'To see', roots: ['see', 'inf'] },
-    'konfuci': { text: 'Confucius (ancient Chinese philosopher)', roots: ['proper noun'] },
+'konfuci': { text: 'Confucius (Chinese philosopher)', roots: ['proper noun'] },
+    'eĉ': { text: 'Even', roots: ['even'] },
+    'plej': { text: 'Most', roots: ['superlative'] },
+    'longa': { text: 'Long', roots: ['long', 'adj'] },
+    'vojaĝo': { text: 'Journey', roots: ['way', 'thing', 'noun'] },
+    'komenciĝas': { text: 'Begins', roots: ['start', 'become', 'pres'] },
+    'per': { text: 'By means of', roots: ['prep'] },
+    'unu': { text: 'One', roots: ['num'] },
+    'paŝo': { text: 'Step', roots: ['step', 'noun'] },
 };
+
 
